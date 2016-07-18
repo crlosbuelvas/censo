8a9afaa7b1dbcf5f32ef971eda0612183c4b1f1f
Ezequiel Bahoque <ezequielbahoquelopez@outlook.com>
2016-07-18 14:27:13 -0500

    avance en las conexiones

--- src/censo/controladores/Conexion.java
+++ src/censo/controladores/Conexion.java
@@ -18,21 +18,33 @@ public class Conexion {
     private Statement statement = null;
     private PreparedStatement preparedStatement = null;
     private ResultSet resultSet = null;
+    private boolean ConnectionFail = false;
 
     public Conexion() {
         try {
+            Class.forName("com.mysql.jdbc.Driver");
             //1. crear conexion
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/suin", "root", "");
             //2. crear objeto statement
             statement = con.createStatement();
         } catch (Exception e1) {
-            try {
-                
-            }catch(Exception e2){
-                
+            //3. si la conexion falla intentamos en otra base temporal y modificamos
+            //   una variable que nos dira del fallo
+            ConnectionFail = true;
+            if(javax.swing.JOptionPane.showConfirmDialog(null, "", "Alerta!", javax.swing.JOptionPane.YES_NO_OPTION) == 0){
+                ConexionLocal();
             }
-        } finally {
         }
-
+    }
+    
+    private void ConexionLocal(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:temp.db");
+            statement = con.createStatement();
+        }catch(ClassNotFoundException | SQLException e2){
+            e2.printStackTrace();
+            System.exit(1);
+        }
     }
 }
