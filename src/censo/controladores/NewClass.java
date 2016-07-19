2a03054e854f0e48d75d02de86ad0e330598c7cf
Ezequiel Bahoque <ezequielbahoquelopez@outlook.com>
2016-07-19 15:22:35 -0500

    comenzamos con los modelos

--- src/censo/controladores/NewClass.java
+++ src/censo/controladores/NewClass.java
@@ -5,33 +5,99 @@
  */
 package censo.controladores;
 
-/**
- *
- * @author Admin
- */
-import javax.swing.JFrame;
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamPanel;
-import com.github.sarxos.webcam.WebcamResolution;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.github.sarxos.webcam.Webcam;
+
 
 public class NewClass {
-    public static void main(String[] args) throws InterruptedException {
 
-		Webcam webcam = Webcam.getDefault();
-		webcam.setViewSize(new Dimension(320, 240));
+	private static final class PlayerPanel extends JPanel {
 
-		WebcamPanel panel = new WebcamPanel(webcam);
-		panel.setFPSDisplayed(true);
-		panel.setDisplayDebugInfo(true);
-		panel.setImageSizeDisplayed(true);
-		panel.setMirrored(true);
+		private static final long serialVersionUID = 1L;
 
-		JFrame window = new JFrame("Test webcam panel");
-		window.add(panel);
-		window.setResizable(true);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.pack();
-		window.setVisible(true);
+		private final Vector<BufferedImage> images;
+		private final Dimension size;
+		private int offset = 0;
+
+		public PlayerPanel(Vector<BufferedImage> images) {
+			super();
+			this.images = images;
+			this.size = new Dimension(images.get(0).getWidth(), images.get(0).getHeight());
+			setPreferredSize(size);
+		}
+
+		public void play() {
+			Thread t = new Thread() {
+
+				@Override
+				public void run() {
+					do {
+						repaint();
+						try {
+							Thread.sleep(100);
+						} catch (InterruptedException e) {
+							return;
+						}
+					} while (++offset < images.size());
+				}
+			};
+			t.setDaemon(true);
+			t.start();
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, getWidth(), getHeight());
+			g.drawImage(images.get(offset), 0, 0, null);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Webcam w = Webcam.getDefault();
+		w.open(true);
+
+		Vector<BufferedImage> images = new Vector<BufferedImage>();
+
+		System.out.println("recording, please wait");
+
+		for (int i = 0; i < 100; i++) {
+			images.add(w.getImage());
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				return;
+			}
+		}
+
+		w.close();
+
+		System.out.println("play");
+
+		PlayerPanel panel = new PlayerPanel(images);
+
+		JFrame f = new JFrame("Take pictures and play example");
+		f.add(panel);
+		f.pack();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+
+		panel.play();
+
+		try {
+			Thread.sleep(100 * images.size());
+		} catch (InterruptedException e) {
+			return;
+		}
 	}
 }
+
